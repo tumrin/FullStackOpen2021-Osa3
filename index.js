a44e2fe4ea40fe112442de63ad083a544ba8fb9c
Tuomas Rinne <tuomas.rin@hotmail.com>
2021-06-15 20:33:28 +0300

    actually fixed port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
-const PORT = Process.env.PORT || 3001
+const PORT = process.env.PORT || 3001
 app.use(express.json())
 morgan.token('json', (req, res) => {return JSON.stringify(req.body)})
 
