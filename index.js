c7da817bb50a3ba4e2edce98603fca9a662a9246
Tuomas Rinne <tuomas.rin@hotmail.com>
2021-07-12 14:44:36 +0300

    Tehtävä 3.15

--- index.js
+++ index.js
@@ -7,49 +7,49 @@ const app = express()
 const Contact = require('./models/mongo')
 const PORT = process.env.PORT
 
-app.use(express.json())
-morgan.token('json', (req, res) => {return JSON.stringify(req.body)})
+morgan.token('json', (req, res) => {
+  return JSON.stringify(req.body)
+})
 
 app.use(express.static('build'))
+app.use(express.json())
 app.use(morgan(':method :url :res[content-length] - :response-time ms :json'))
 app.use(cors())
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
+  Contact.findByIdAndRemove(request.params.id).then((result) => {
     response.status(204).end()
-    
+  })
 })
+
 app.post('/api/persons', (request, response) => {
   const person = request.body
-  if(!person.name || !person.number){
-    response.status(400).json({error:'both name and number required'})
-  }
-  else{
+  if (!person.name || !person.number) {
+    response.status(400).json({ error: 'both name and number required' })
+  } else {
     const contact = new Contact({
       name: person.name,
       number: person.number,
-      id:Math.floor(Math.random() * 1000)
+      id: Math.floor(Math.random() * 1000)
     })
-    contact.save().then(saved => {
+    contact.save().then((saved) => {
       response.json(saved)
     })
   }
 })
 
 app.get('/api/persons', (request, response) => {
-    Contact.find({}).then(contacts => response.json(contacts))
+  Contact.find({}).then((contacts) => response.json(contacts))
 })
 app.get('/info', (request, response) => {
-    response.send(`Phonebook has ${persons.length} people<br/> ${new Date()}`)
+  response.send(`Phonebook has ${persons.length} people<br/> ${new Date()}`)
 })
 app.get('/api/persons/:id', (request, response) => {
-    Contact.findById(request.params.id).then(contact => {
-      response.json(contact)
-    })
+  Contact.findById(request.params.id).then((contact) => {
+    response.json(contact)
+  })
 })
 
-
-app.listen(PORT, ()=>{
-    console.log(`Server up on port: ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server up on port: ${PORT}`)
 })
