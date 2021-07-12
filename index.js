23d1d739df04422bcd8f4f056d276c4d6d453817
Tuomas Rinne <tuomas.rin@hotmail.com>
2021-07-12 15:18:46 +0300

    3.16-3.18

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ morgan.token('json', (req, res) => {
   return JSON.stringify(req.body)
 })
 
+const errorHandler = (error, request, response,next) => {
+  console.error(error.message)
+
+  if(error.name === 'CastError') {
+    return response.status(400).send({error: 'malformatted id'})
+  }
+  next(error)
+}
+
 app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan(':method :url :res[content-length] - :response-time ms :json'))
@@ -20,6 +29,7 @@ app.delete('/api/persons/:id', (request, response) => {
   Contact.findByIdAndRemove(request.params.id).then((result) => {
     response.status(204).end()
   })
+  .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -35,21 +45,40 @@ app.post('/api/persons', (request, response) => {
     contact.save().then((saved) => {
       response.json(saved)
     })
+    .catch(error => next(error))
   }
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const contact = {
+    name: body.name,
+    number: body.number
+  }
+  Contact.findByIdAndUpdate(request.params.id, contact, { new:true })
+  .then(updatedContact => {
+    response.json(updatedContact)
+  })
+  .catch(error => next(error))
 })
 
 app.get('/api/persons', (request, response) => {
   Contact.find({}).then((contacts) => response.json(contacts))
 })
 app.get('/info', (request, response) => {
-  response.send(`Phonebook has ${persons.length} people<br/> ${new Date()}`)
+  Contact.find({}).then(contacts =>   response.send(`Phonebook has ${contacts.length} people<br/> ${new Date()}`))
+
 })
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Contact.findById(request.params.id).then((contact) => {
     response.json(contact)
   })
+  .catch(error => next(error))
 })
 
 app.listen(PORT, () => {
   console.log(`Server up on port: ${PORT}`)
 })
+
+app.use(errorHandler)
